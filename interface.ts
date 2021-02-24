5f27f840bad35429a88d306e20d70971f3aaa5dd
Ang3l1t0 <1546@holbertonschool.com>
2021-02-24 00:29:51 -0500

    interfaces

--- interface.ts
+++ interface.ts
@@ -29,4 +29,46 @@ mostrarCoche({ marca: 'Renault', modelo: '4' });
 mostrarCoche({ marca: 'Renault', modelo: '4', color: 'Verde' });
 
 // Propiedades de solo lectura
+interface Punto {
+    readonly x: number;
+    readonly y: number;
+}
 
+let p1: Punto = { x: 21, y: 22 };
+// p1.x = 32; ---> Cannot assign to 'x' because it is a read-only property.
+
+//Definir una funcion
+interface Busqueda {
+    (valor: string, buscar: string): boolean;
+}
+
+const miBusqueda: Busqueda = (v: string, b: string): boolean => {
+    const result = v.search(b);
+    return result > -1;
+}
+
+console.log(miBusqueda('hola', 'ola'));
+
+//Interfaz en una clase
+interface Humano {
+    nombre: string;
+    edad: number;
+}
+
+class Adulto implements Humano {
+    nombre: string;
+    edad: number;
+    coche: boolean;
+
+    constructor(pNombre: string, pEdad: number, pCoche: boolean) {
+        this.nombre = pNombre;
+        this.edad = pEdad;
+        this.coche = pCoche;
+    }
+}
+
+class Nino implements Humano {
+    nombre: string;
+    edad: number;
+    colegio: string;
+}
