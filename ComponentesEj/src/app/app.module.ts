bfdddbbe5d8c3326f191efa86652ad220c6dae24
Ang3l1t0 <1546@holbertonschool.com>
2021-03-01 14:48:50 -0500

    componente calculadora

--- ComponentesEj/src/app/app.module.ts
+++ ComponentesEj/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SumarComponent } from './components/sumar/sumar.component';
 import { CronoComponent } from './components/crono/crono.component';
-import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SumarComponent,
     CronoComponent,
-    CalculadoraComponent
+    CalculadoraComponent,
   ],
   imports: [
     BrowserModule
