4cf737e0e38590265d5df14938b1373de77f9477
Ang3l1t0 <1546@holbertonschool.com>
2021-03-01 10:10:16 -0500

    more events

--- ComponentesEj/src/app/app.module.ts
+++ ComponentesEj/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SumarComponent } from './components/sumar/sumar.component';
+import { CronoComponent } from './components/crono/crono.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SumarComponent
+    SumarComponent,
+    CronoComponent
   ],
   imports: [
     BrowserModule
--- MiPrimeraApp/src/app/event/event.component.ts
+++ MiPrimeraApp/src/app/event/event.component.ts
@@ -7,12 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventComponent implements OnInit {
 
-  constructor() { }
+  mensaje: string;
+
+  constructor() {
+    this.mensaje = "Este es el mensaje predeterminado"
+  }
 
   ngOnInit(): void {
   }
 
   onClick() {
     console.log('se ha pulsado el boton');
+  }
+
+  onChange($event) {
+    console.log($event.target)
+  }
+
+  onMouseEnter() {
+    //console.log('Entro en el div')
+    this.mensaje = "Entraste al div"
+  }
+
+  onMouseOut() {
+    this.mensaje = "Estas afuera del div"
   }
 }
