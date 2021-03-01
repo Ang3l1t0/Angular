681460532e5e0073adb96851ee1e51c35abfd20a
Ang3l1t0 <1546@holbertonschool.com>
2021-03-01 11:19:59 -0500

    mouse event

--- ComponentesEj/src/app/app.module.ts
+++ ComponentesEj/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SumarComponent } from './components/sumar/sumar.component';
 import { CronoComponent } from './components/crono/crono.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SumarComponent,
-    CronoComponent
+    CronoComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule
